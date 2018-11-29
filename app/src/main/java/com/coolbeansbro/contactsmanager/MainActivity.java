6fcd7a770eae59becb763c724b707570ccbe8bd7
UTDahmad <38578797+UTDahmad@users.noreply.github.com>
2018-11-28 20:11:30 -0600

    Added search bar with keyboard pop up

--- app/src/main/java/com/coolbeansbro/contactsmanager/MainActivity.java
+++ app/src/main/java/com/coolbeansbro/contactsmanager/MainActivity.java
@@ -2,8 +2,12 @@ package com.coolbeansbro.contactsmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
@@ -21,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
         all_contacts_search = (ListView) findViewById(R.id.all_contacts_search);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater search = getMenuInflater();
+        search.inflate(R.menu.all_contacts, menu);
+        MenuItem searchBar = menu.findItem(R.id.all_contacts_search);
+        SearchView searchContacts = (SearchView)searchBar.getActionView();
+
+        searchContacts.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
