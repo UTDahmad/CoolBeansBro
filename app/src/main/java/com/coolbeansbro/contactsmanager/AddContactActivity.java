34ddbcc5803d334bcbed7cc5b896ef12bf445af6
UTDahmad <38578797+UTDahmad@users.noreply.github.com>
2018-11-29 15:44:57 -0600

    Fixed some errors and warnings in xml files

--- app/src/main/java/com/coolbeansbro/contactsmanager/AddContactActivity.java
+++ app/src/main/java/com/coolbeansbro/contactsmanager/AddContactActivity.java
@@ -1,5 +1,6 @@
 package com.coolbeansbro.contactsmanager;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class AddContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+
     }
 }
