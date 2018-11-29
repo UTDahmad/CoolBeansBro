23e7cba19dce67074a2b011d2dc7ba9e51d88601
UTDahmad <38578797+UTDahmad@users.noreply.github.com>
2018-11-28 19:04:10 -0600

    Removed some redundant code, tested app, made minor modifications to icons

--- app/src/main/java/com/coolbeansbro/contactsmanager/MainActivity.java
+++ app/src/main/java/com/coolbeansbro/contactsmanager/MainActivity.java
@@ -2,17 +2,24 @@ package com.coolbeansbro.contactsmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView main_search;
+    ListView all_contacts_search;
 
-    ArrayAdapter<>
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        all_contacts_search = (ListView) findViewById(R.id.all_contacts_search);
+
+
     }
 }
