057f65ec2c67ce5ed037cc255605b7a1d3c8f46c
UTDahmad <38578797+UTDahmad@users.noreply.github.com>
2018-11-29 01:47:01 -0600

    Added strings, modified xml for adding a contact page

--- app/src/main/java/com/coolbeansbro/contactsmanager/MainActivity.java
+++ app/src/main/java/com/coolbeansbro/contactsmanager/MainActivity.java
@@ -1,17 +1,24 @@
 package com.coolbeansbro.contactsmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     ListView all_contacts_search;
 
@@ -21,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        Button addContactButton = findViewById(R.id.floatingAddContact);
+
+        addContactButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddContactActivity.class));
+            }
+        });*/
+
+
 
         all_contacts_search = (ListView) findViewById(R.id.all_contacts_search);
 
@@ -48,5 +67,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void AddContactActivity(View v)
+    {
+        startActivity(new Intent(MainActivity.this, AddContactActivity.class));
     }
 }
