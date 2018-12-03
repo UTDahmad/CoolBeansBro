d3c491a29cec3ce72c2ba4d27e28af697f42e8b4
UTDahmad <38578797+UTDahmad@users.noreply.github.com>
2018-12-03 02:52:59 -0600

    changed add contact page layout to horizontal instead of vertical and added more information to backwards compatible version of xml file

--- app/src/main/java/com/coolbeansbro/contactsmanager/DatabaseController.java
+++ app/src/main/java/com/coolbeansbro/contactsmanager/DatabaseController.java
@@ -3,14 +3,14 @@ package com.coolbeansbro.contactsmanager;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
+/*
 public class DatabaseController extends SQLiteOpenHelper {
 
 
 
 
     public DatabaseController(Context context) {
-        
+
     }
 
     @Override
@@ -23,3 +23,4 @@ public class DatabaseController extends SQLiteOpenHelper {
 
     }
 }
+*/
