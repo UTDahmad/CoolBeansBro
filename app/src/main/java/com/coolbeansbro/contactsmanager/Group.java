e1659a9f96ccebf4c45f0fd9313750ece7174d20
YeswanthBogireddy <31995926+YeswanthBogireddy@users.noreply.github.com>
2018-12-04 08:09:10 -0600

    Created class for contactdisplay

--- app/src/main/java/com/coolbeansbro/contactsmanager/Group.java
+++ app/src/main/java/com/coolbeansbro/contactsmanager/Group.java
@@ -1,10 +1,10 @@
 package com.coolbeansbro.contactsmanager;
 
-ContentValues groupValues;
+/*ContentValues groupValues;
         create group()
         {
         ContentResolver cr = this.getContentResolver();
         groupValues = new ContentValues();
         groupValues.put(ContactsContract.Groups.TITLE, "MyContactGroup");
         cr.insert(ContactsContract.Groups.CONTENT_URI, groupValues);
-        }
+        }*/
